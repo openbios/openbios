1ebb8a3b5003bd1692433fe07734326db45ff33e
Stefan Reinauer <stepan@openbios.org>
2007-04-09 12:19:41 +0000

    fix from blueswirl
    git-svn-id: svn://coreboot.org/openbios/openbios-devel@119 f158a5a8-5612-0410-a976-696ce0be7e32

--- drivers/obio.c
+++ drivers/obio.c
@@ -341,7 +341,8 @@ ob_nvram_init(unsigned long base, unsigned long offset)
     kernel_image = nv_info.kernel_image;
     kernel_size = nv_info.kernel_size;
     size = nv_info.cmdline_size;
-    if (size > OBIO_CMDLINE_MAX) size = OBIO_CMDLINE_MAX;
+    if (size > OBIO_CMDLINE_MAX - 1)
+        size = OBIO_CMDLINE_MAX - 1;
     memcpy(obio_cmdline, nv_info.cmdline, size);
     obio_cmdline[size] = '\0';
     cmdline = obio_cmdline;
