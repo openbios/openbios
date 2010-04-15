80cead52ebd56e2eb45c9c4ef622f59e43170b22
Blue Swirl <blauwirbel@gmail.com>
2010-04-15 16:42:59 +0000

    Remove unused strstr()
    Signed-off-by: Blue Swirl <blauwirbel@gmail.com>
    
    git-svn-id: svn://coreboot.org/openbios/trunk/openbios-devel@746 f158a5a8-5612-0410-a976-696ce0be7e32

--- libc/string.c
+++ libc/string.c
@@ -347,28 +347,6 @@ int memcmp(const void * cs,const void * ct,size_t count)
 }
 
 /**
- * strstr - Find the first substring in a %NUL terminated string
- * @s1: The string to be searched
- * @s2: The string to search for
- */
-char * strstr(const char * s1,const char * s2)
-{
-	int l1, l2;
-
-	l2 = strlen(s2);
-	if (!l2)
-		return (char *) s1;
-	l1 = strlen(s1);
-	while (l1 >= l2) {
-		l1--;
-		if (!memcmp(s1,s2,l2))
-			return (char *) s1;
-		s1++;
-	}
-	return NULL;
-}
-
-/**
  * memchr - Find a character in an area of memory.
  * @s: The memory area
  * @c: The byte to search for
