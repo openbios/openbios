d1c94b88f45b3ec0977014d769902c93608f83d5
Andreas Färber <andreas.faerber@web.de>
2010-10-30 22:49:20 +0000

    ppc: Add support for -cpu 970
    The 970fx is supported, which happens to be the default CPU for -M mac99.
    The IBM JS20 had a dual 970 though, and -cpu 970 leads to a busy loop:
    
    >> Unknown cpu (pvr 390000), freezing!
    
    Add a definition for the 970.
    
    Signed-off-by: Andreas Färber <andreas.faerber@web.de>
    
    
    git-svn-id: svn://coreboot.org/openbios/trunk/openbios-devel@934 f158a5a8-5612-0410-a976-696ce0be7e32

--- arch/ppc/qemu/init.c
+++ arch/ppc/qemu/init.c
@@ -472,6 +472,18 @@ static const struct cpudef ppc_defs[] = {
         .clock_frequency = 0x1dcd6500,
         .initfn = cpu_g4_init,
     },
+    {
+        .iu_version = 0x00390000,
+        .name = "PowerPC,970",
+        .icache_size = 0x10000,
+        .dcache_size = 0x8000,
+        .icache_sets = 0x200,
+        .dcache_sets = 0x80,
+        .icache_block_size = 0x80,
+        .dcache_block_size = 0x80,
+        .clock_frequency = 0x5f5e1000,
+        .initfn = cpu_970_init,
+    },
     { // XXX find out real values
         .iu_version = 0x003C0000,
         .name = "PowerPC,970FX",
