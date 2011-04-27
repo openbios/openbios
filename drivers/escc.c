b8af4b8bc5811a5f39decdd5eb1570250a8cfcd8
Mark Cave-Ayland <mark.cave-ayland@siriusit.co.uk>
2011-04-27 19:20:06 +0000

    Set properties to disable carrier detection on the serial ports for SPARC32.
    This prevents the Solaris 8 kernel from freezing during boot after detecting
    the serial ports.
    
    Signed-off-by: Mark Cave-Ayland <mark.cave-ayland@siriusit.co.uk>
    
    git-svn-id: svn://coreboot.org/openbios/trunk/openbios-devel@1036 f158a5a8-5612-0410-a976-696ce0be7e32

--- drivers/escc.c
+++ drivers/escc.c
@@ -342,6 +342,16 @@ ob_zs_init(phys_addr_t base, uint64_t offset, int intr, int slave, int keyboard)
 
     ob_intr(intr);
 
+    PUSH(0);
+    PUSH(0);
+    push_str("port-a-ignore-cd");
+    fword("property");
+
+    PUSH(0);
+    PUSH(0);
+    push_str("port-b-ignore-cd");
+    fword("property");
+
     fword("finish-device");
 
     snprintf(nodebuff, sizeof(nodebuff), "/obio/zs@0,%x",
