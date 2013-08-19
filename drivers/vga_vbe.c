348ae7503c85a3dc37fe37a2987f8d700acdcc92
Mark Cave-Ayland <mark.cave-ayland@ilande.co.uk>
2013-08-19 07:40:01 +0000

    vga: move VGA initialisation from C to Forth
    This includes the low-level VGA register accesses and also the package
    properties.
    
    Signed-off-by: Mark Cave-Ayland <mark.cave-ayland@ilande.co.uk>
    
    git-svn-id: svn://coreboot.org/openbios/trunk/openbios-devel@1205 f158a5a8-5612-0410-a976-696ce0be7e32

--- drivers/vga_vbe.c
+++ drivers/vga_vbe.c
@@ -51,74 +51,6 @@
 #define VBE_DISPI_LFB_ENABLED           0x40
 #define VBE_DISPI_NOCLEARMEM            0x80
 
-static void vbe_outw(int index, int val)
-{
-    outw(index, 0x1ce);
-    outw(val, 0x1d0);
-}
-
-/* for depth = 8 mode, set a hardware palette entry */
-void vga_set_color(int i, unsigned int r, unsigned int g, unsigned int b)
-{
-    r &= 0xff;
-    g &= 0xff;
-    b &= 0xff;
-    outb(i, 0x3c8);
-    outb(r >> 2, 0x3c9);
-    outb(g >> 2, 0x3c9);
-    outb(b >> 2, 0x3c9);
-}
-
-/* build standard RGB palette */
-static void vga_build_rgb_palette(void)
-{
-    static const uint8_t pal_value[6] = { 0x00, 0x33, 0x66, 0x99, 0xcc, 0xff };
-    int i, r, g, b;
-
-    i = 0;
-    for(r = 0; r < 6; r++) {
-        for(g = 0; g < 6; g++) {
-            for(b = 0; b < 6; b++) {
-                vga_set_color(i, pal_value[r], pal_value[g], pal_value[b]);
-                i++;
-            }
-        }
-    }
-}
-
-/* depth = 8, 15, 16 or 32 */
-void vga_vbe_set_mode(int width, int height, int depth)
-{
-    outb(0x00, 0x3c0); /* enable blanking */
-    vbe_outw(VBE_DISPI_INDEX_ENABLE, VBE_DISPI_DISABLED);
-    vbe_outw(VBE_DISPI_INDEX_X_OFFSET, 0);
-    vbe_outw(VBE_DISPI_INDEX_Y_OFFSET, 0);
-    vbe_outw(VBE_DISPI_INDEX_XRES, width);
-    vbe_outw(VBE_DISPI_INDEX_YRES, height);
-    vbe_outw(VBE_DISPI_INDEX_BPP, depth);
-    vbe_outw(VBE_DISPI_INDEX_ENABLE, VBE_DISPI_ENABLED);
-    outb(0x00, 0x3c0);
-    outb(0x20, 0x3c0); /* disable blanking */
-
-    if (depth == 8)
-        vga_build_rgb_palette();
-}
-
-/* Low-level Forth accessor to update VGA color registers */
-
-/* ( r g b index -- ) */
-static
-void vga_hw_set_color(void)
-{
-    int index = POP();
-    int b = POP();
-    int g = POP();
-    int r = POP();
-
-    vga_set_color(index, (r & 0xff),
-        (g & 0xff),
-        (b & 0xff));
-}
 
 void vga_vbe_init(const char *path, unsigned long fb, uint32_t fb_size,
                   unsigned long rom, uint32_t rom_size)
@@ -136,23 +68,12 @@ void vga_vbe_init(const char *path, unsigned long fb, uint32_t fb_size,
 #endif
 
 	setup_video(phys, fb);
-	vga_vbe_set_mode(VIDEO_DICT_VALUE(video.w),
-			 VIDEO_DICT_VALUE(video.h),
-			 VIDEO_DICT_VALUE(video.depth));
 
 #if 0
     ph = find_dev(path);
 #else
     ph = get_cur_dev();
 #endif
-
-	bind_func("hw-set-color", vga_hw_set_color);
-
-	set_int_property(ph, "width", VIDEO_DICT_VALUE(video.w));
-	set_int_property(ph, "height", VIDEO_DICT_VALUE(video.h));
-	set_int_property(ph, "depth", VIDEO_DICT_VALUE(video.depth));
-	set_int_property(ph, "linebytes", VIDEO_DICT_VALUE(video.rb));
-	set_int_property(ph, "address", (u32)(fb & ~0x0000000F));
 
 	molvideo_init();
 
