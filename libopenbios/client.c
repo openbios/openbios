70fe98d65a13ac129241111a565366cecb9f0c5c
Mark Cave-Ayland <mark.cave-ayland@ilande.co.uk>
2014-01-13 09:46:43 +0000

    client.c: stop clients from being able to invoke stack underflow
    Clients that incorrectly requested too many return arguments for a client
    interface call could cause a Forth stack underflow (this was discovered due
    to a bug in NetBSD SPARC64).
    
    Ensure that we can never POP() beyond the original position of the stack from
    before the client interface call.
    
    Signed-off-by: Mark Cave-Ayland <mark.cave-ayland@ilande.co.uk>
    
    git-svn-id: svn://coreboot.org/openbios/trunk/openbios-devel@1250 f158a5a8-5612-0410-a976-696ce0be7e32

--- libopenbios/client.c
+++ libopenbios/client.c
@@ -244,41 +244,43 @@ static void dump_return(prom_args_t *pb)
 
 /* call-method, interpret */
 static int
-handle_calls( prom_args_t *pb )
+handle_calls(prom_args_t *pb)
 {
-	int i, dstacksave = dstackcnt;
-	prom_uarg_t val;
+	int i, j, dstacksave;
+	ucell val;
 
 #ifdef DEBUG_CIF
 	printk("%s %s ([" FMT_prom_arg "] -- [" FMT_prom_arg "])\n",
 		get_service(pb), arg2pointer(pb->args[0]), pb->nargs, pb->nret);
 #endif
 
+	dstacksave = dstackcnt;
 	for (i = pb->nargs - 1; i >= 0; i--)
 		PUSH(pb->args[i]);
 
 	push_str(get_service(pb));
 	fword("client-call-iface");
 
-	/* Drop the return code from client-call-iface (status is handled by the 
-	   catch result which is the first parameter below) */
-	POP();
+	/* If the catch result is non-zero, restore stack and exit */
+	val = POP();
+	if (val) {
+		dstackcnt = dstacksave;
+		return 0;
+	}
 
-	for (i = 0; i < pb->nret; i++) {
-                val = POP();
-		pb->args[pb->nargs + i] = val;
-
-		/* don't pop args if an exception occured */
-		if (!i && val)
-			break;
+	j = dstackcnt;
+	for (i = 0; i < pb->nret; i++, j--) {
+                if (dstackcnt > dstacksave) {
+			pb->args[pb->nargs + i] = POP();
+		}
 	}
 
 #ifdef DEBUG_CIF
 	/* useful for debug but not necessarily an error */
-	if (i != pb->nret || dstackcnt != dstacksave) {
+	if (j != dstacksave) {
 		printk("%s '%s': possible argument error (" FMT_prom_arg "--" FMT_prom_arg ") got %d\n",
 			get_service(pb), arg2pointer(pb->args[0]),
-			pb->nargs - 2, pb->nret, i);
+			pb->nargs - 2, pb->nret, j - dstacksave);
 	}
 
 	printk("handle_calls return:");
@@ -293,10 +295,11 @@ handle_calls( prom_args_t *pb )
 }
 
 int
-of_client_interface( int *params )
+of_client_interface(int *params)
 {
 	prom_args_t *pb = (prom_args_t*)params;
-	int val, i, dstacksave;
+	ucell val;
+	int i, j, dstacksave;
 
 	if (pb->nargs < 0 || pb->nret < 0 ||
             pb->nargs + pb->nret > PROM_MAX_ARGS)
@@ -317,34 +320,41 @@ of_client_interface( int *params )
 	push_str(get_service(pb));
 	fword("client-iface");
 
-	if (val=POP()) {
-		dstackcnt = dstacksave;
-		if (val == -1)
+	val = POP();
+	if (val) {
+		if (val == -1) {
 			printk("Unimplemented service %s ([" FMT_prom_arg "] -- [" FMT_prom_arg "])\n",
-			       get_service(pb), pb->nargs, pb->nret);
+				get_service(pb), pb->nargs, pb->nret);
+		} else {
 #ifdef DEBUG_CIF
-		else
-			printk("ERROR!\n");
+			printk("Error calling client interface: " FMT_ucellx "\n", val);
 #endif
+		}
+
+		dstackcnt = dstacksave;
 		return -1;
 	}
 
-	for (i = 0; i < pb->nret ; i++)
-		pb->args[pb->nargs + i] = POP();
-
-	if (dstackcnt != dstacksave) {
-#ifdef DEBUG_CIF
-		printk("service %s: possible argument error (%d %d)\n",
-		       get_service(pb), i, dstackcnt - dstacksave);
-#endif
-		/* Some clients request less parameters than the CIF method
-		returns, e.g. getprop with OpenSolaris. Hence we drop any
-		stack parameters after issuing a warning above */
-		dstackcnt = dstacksave;
+	j = dstackcnt;
+	for (i = 0; i < pb->nret; i++, j--) {
+		if (dstackcnt > dstacksave) {
+			pb->args[pb->nargs + i] = POP();
+		}
 	}
 
 #ifdef DEBUG_CIF
+	if (j != dstacksave) {
+		printk("service %s: possible argument error (%d %d)\n",
+		       get_service(pb), i, j - dstacksave);
+
+		/* Some clients request less parameters than the CIF method
+		returns, e.g. getprop with OpenSolaris. Hence we drop any
+		stack parameters on exit after issuing a warning above */
+	}
+
 	dump_return(pb);
 #endif
+
+	dstackcnt = dstacksave;
 	return 0;
 }
