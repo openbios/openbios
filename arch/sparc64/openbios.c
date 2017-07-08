447888871dc3692c6e5559b4692e43d9ed4ed071
Mark Cave-Ayland <mark.cave-ayland@ilande.co.uk>
2017-07-08 21:35:48 +0100

    SPARC64: increase size of PCI memory space
    On a real machine the PCI memory space should be 10 times as large, however that
    takes us to a 64-bit value which isn't directly supported by OpenBIOS. Increase
    the PCI memory space by an amount which allows Linux to allocate the IOMMU correctly.
    
    Signed-off-by: Mark Cave-Ayland <mark.cave-ayland@ilande.co.uk>

--- arch/sparc64/openbios.c
+++ arch/sparc64/openbios.c
@@ -61,13 +61,13 @@ static const struct hwdef hwdefs[] = {
             .cfg_len = 0x2000000,
             .host_pci_base = APB_MEM_BASE,
             .pci_mem_base = 0x100000, /* avoid VGA at 0xa0000 */
-            .mem_len = 0x10000000,
+            .mem_len = 0xf0000000,
             .io_base = APB_SPECIAL_BASE + 0x2000000ULL, // PCI Bus I/O space
             .io_len = 0x10000,
             .host_ranges = {
                 { .type = CONFIGURATION_SPACE, .parentaddr = 0, .childaddr = APB_SPECIAL_BASE + 0x1000000ULL, .len = 0x2000000 },
                 { .type = IO_SPACE, .parentaddr = 0, .childaddr = APB_SPECIAL_BASE + 0x2000000ULL, .len = 0x10000 },
-                { .type = MEMORY_SPACE_32, .parentaddr = 0x100000, .childaddr = APB_MEM_BASE + 0x100000ULL, .len = 0x10000000 },
+                { .type = MEMORY_SPACE_32, .parentaddr = 0x100000, .childaddr = APB_MEM_BASE + 0x100000ULL, .len = 0xf0000000 },
                 { .type = 0, .parentaddr = 0, .childaddr = 0, .len = 0 }
             },
             .irqs = { 0, 1, 2, 3 },
